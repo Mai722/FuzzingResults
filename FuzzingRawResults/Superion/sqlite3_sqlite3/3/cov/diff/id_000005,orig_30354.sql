a13fcc2c339b589e06d313731e4954fe8d40ce5d
Mai722 <45427021+Mai722@users.noreply.github.com>
2021-12-01 13:08:43 +0100

    Add files via upload

---
+++ FuzzingRawResults/Superion/sqlite3_sqlite3/3/cov/diff/id_000005,orig_30354.sql
@@ -0,0 +1,7 @@
+diff id:000004,orig:30334.sql -> id:000005,orig:30354.sql
+Src file: /Superion/code1/sqlite3.c
+  New 'line' coverage: 130087
+  New 'line' coverage: 131166
+  New 'line' coverage: 131629
+  New 'line' coverage: 131631
+  New 'line' coverage: 153351
---
+++ FuzzingRawResults/Superion/sqlite3_sqlite3/3/cov/diff/id_000010,orig_31732.sql
@@ -0,0 +1,8 @@
+diff id:000009,orig:31680.sql -> id:000010,orig:31732.sql
+Src file: /Superion/code1/sqlite3.c
+  New 'line' coverage: 107795
+  New 'line' coverage: 96459
+  New 'line' coverage: 96460
+  New 'line' coverage: 96463
+  New 'line' coverage: 96578
+  New 'line' coverage: 96580
