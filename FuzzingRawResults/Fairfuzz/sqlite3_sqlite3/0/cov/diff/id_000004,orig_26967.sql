84faea116d95806496fbefe161b71dbcffdd0a84
Mai722 <45427021+Mai722@users.noreply.github.com>
2021-12-01 11:30:24 +0100

    Add files via upload

---
+++ FuzzingRawResults/Fairfuzz/sqlite3_sqlite3/0/cov/diff/id_000004,orig_26967.sql
@@ -0,0 +1,18 @@
+diff id:000003,orig:26849.sql -> id:000004,orig:26967.sql
+Src file: /afl-rb/code1/sqlite3.c
+  New 'function' coverage: yy_syntax_error.isra.334()
+  New 'line' coverage: 154391
+  New 'line' coverage: 154397
+  New 'line' coverage: 154402
+  New 'line' coverage: 154403
+  New 'line' coverage: 154409
+  New 'line' coverage: 154410
+  New 'line' coverage: 154510
+  New 'line' coverage: 154583
+  New 'line' coverage: 154584
+  New 'line' coverage: 154585
+  New 'line' coverage: 28232
+  New 'line' coverage: 28234
+  New 'line' coverage: 28235
+  New 'line' coverage: 28237
+  New 'line' coverage: 28238
