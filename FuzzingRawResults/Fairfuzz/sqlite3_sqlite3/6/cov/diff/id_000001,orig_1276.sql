3d7afddfee4f64aebec6e1198ea6d8613f24f7e7
Mai722 <45427021+Mai722@users.noreply.github.com>
2021-12-01 11:36:16 +0100

    Add files via upload

---
+++ FuzzingRawResults/Fairfuzz/sqlite3_sqlite3/6/cov/diff/id_000001,orig_1276.sql
@@ -0,0 +1,8 @@
+diff id:000000,orig:1206.sql -> id:000001,orig:1276.sql
+Src file: /afl-rb/code1/shell.c
+  New 'line' coverage: 18340
+  New 'line' coverage: 18346
+  New 'line' coverage: 599
+  New 'line' coverage: 600
+Src file: /afl-rb/code1/sqlite3.c
+  New 'line' coverage: 155490
---
+++ FuzzingRawResults/Fairfuzz/sqlite3_sqlite3/6/cov/diff/id_000018,orig_4236.sql
@@ -0,0 +1,16 @@
+diff id:000017,orig:4102.sql -> id:000018,orig:4236.sql
+Src file: /afl-rb/code1/sqlite3.c
+  New 'function' coverage: compileoptionusedFunc()
+  New 'function' coverage: substrFunc()
+  New 'line' coverage: 114235
+  New 'line' coverage: 114245
+  New 'line' coverage: 114248
+  New 'line' coverage: 114910
+  New 'line' coverage: 114922
+  New 'line' coverage: 114925
+  New 'line' coverage: 155308
+  New 'line' coverage: 155309
+  New 'line' coverage: 155310
+  New 'line' coverage: 85954
+  New 'line' coverage: 85955
+  New 'line' coverage: 85957
