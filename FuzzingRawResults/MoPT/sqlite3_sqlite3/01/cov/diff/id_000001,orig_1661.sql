6d81cd1e7d0c42ffcc3ac2f8ea157bc70bee6f62
Mai722 <45427021+Mai722@users.noreply.github.com>
2021-12-01 16:07:12 +0100

    Add files via upload

---
+++ FuzzingRawResults/MoPT/sqlite3_sqlite3/01/cov/diff/id_000001,orig_1661.sql
@@ -0,0 +1,11 @@
+diff id:000000,orig:1276.sql -> id:000001,orig:1661.sql
+Src file: /MOpt-AFL/MOpt/code1/shell.c
+  New 'line' coverage: 599
+  New 'line' coverage: 600
+Src file: /MOpt-AFL/MOpt/code1/sqlite3.c
+  New 'line' coverage: 156059
+  New 'line' coverage: 156060
+  New 'line' coverage: 156067
+  New 'line' coverage: 156068
+  New 'line' coverage: 156070
+  New 'line' coverage: 156072
