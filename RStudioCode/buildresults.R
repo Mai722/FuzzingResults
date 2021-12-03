bd8fa4350ea351b6dd1834117ac9e6b61430e087
Mai722 <45427021+Mai722@users.noreply.github.com>
2021-12-03 08:15:42 +0100

    Add files via upload

---
+++ RStudioCode/buildresults.R
@@ -0,0 +1,16 @@
+
+library(ggplot2)
+
+
+#write the path where the csv file is
+data_AFLfast <- read.csv("C:/Users/Final/data_AFLfast.csv")
+data_AFLgo <- read.csv("C:/Users/Final/data_AFLgo.csv")
+data_AFLplusplus <- read.csv("C:/Users/Final/data_AFLplusplus.csv")
+data_fairfuzz <- read.csv("C:/Users/Final/data_fairfuzz.csv")
+data_learnafl <- read.csv("C:/Users/Final/data_learnafl.csv")
+data_mopt <- read.csv("C:/Users/Final/data_mopt.csv")
+data_superion <- read.csv("C:/Users/data_superion.csv")
+
+results <- rbind(data_AFLfast, data_AFLgo, data_AFLplusplus, data_fairfuzz, data_learnafl, data_mopt, data_superion)
+
+write.csv(results, file="C:/Users/results.csv")
